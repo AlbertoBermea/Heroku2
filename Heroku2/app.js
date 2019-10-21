0b91ad9198d4b00e3f4c85a8ccd49ad75ec13bf2
AlbertoBermea <alberto-bermi@hotmail.com>
2019-10-20 22:33:54 -0500

    ewd

---
+++ Heroku2/app.js
@@ -0,0 +1,27 @@
+
+const port = process.env.PORT || 3000
+
+const express = require('express')
+
+
+const app = express()
+
+const mapbax_token = process.env.MAPBOX_TOKEN || 'pk.eyJ1IjoiYWJlcm1lYSIsImEiOiJjazFpbHU3ZG0wNHBkM25xN2YxcHhvbDkzIn0.fdLNntVLgdoDuW5f6uSFsw'
+
+const weather_token ='9bb95c5190098c9e453124f291b905a9'
+
+app.get('/', function(req, res) {
+    res.send({ 
+      greeting: mapbax_token
+    })
+  })
+
+
+
+app.listen(port, function() {
+  console.log('Up and running!')
+})
+  
+
+
+
